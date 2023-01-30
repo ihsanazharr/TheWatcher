6e11287edd442d7833a3c150a5d1e844cc7788ed
Ihsan Ertansa Azhar <ihsan.azhar41@gmail.com>
2023-01-30 08:32:05 +0700

    Fixing Reset Password Activity

--- app/src/main/java/com/ihsan/thewatcher/ResetPasswordActivity.kt
+++ app/src/main/java/com/ihsan/thewatcher/ResetPasswordActivity.kt
@@ -1,11 +1,51 @@
 package com.ihsan.thewatcher
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.ihsan.thewatcher.databinding.ActivityResetPasswordBinding
 
 class ResetPasswordActivity : AppCompatActivity() {
+
+    lateinit var auth : FirebaseAuth
+    lateinit var binding : ActivityResetPasswordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reset_password)
+        binding = ActivityResetPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnReset.setOnClickListener {
+            val email = binding.edtEmailReset.text.toString()
+            val edtEmail = binding.edtEmailReset
+
+            if (email.isEmpty()){
+                edtEmail.error = "Email tidak boleh kosong"
+                edtEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                edtEmail.error = "Email tidak valid"
+                edtEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            FirebaseAuth.getInstance().sendPasswordResetEmail(email).addOnCompleteListener {
+                if (it.isSuccessful){
+                    Toast.makeText(this, "Email Reset Password telah dikirim", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }else{
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
     }
+
 }
